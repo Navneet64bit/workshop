fbcf88fc0544167570458e8783adfcf68da757b9
navneet64bit <navneetkumar1849@gmail.com>
2024-12-21 15:53:46 +0530

    changes after magga

--- sample.java
+++ sample.java
@@ -3,5 +3,7 @@ public class sample
     public static void main(String[] args)
     {
         System.out.println("hello World");
+        System.out.println("my magga");
+        System.out.println("mnfjaj");
     }
 }
